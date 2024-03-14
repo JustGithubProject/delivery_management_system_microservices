125ca5af7275bb9ee4ab60af0dbaa3ab5d64b9ce
JustGithubProject <Kropivadanil@gmail.com>
2024-03-14 21:04:08 +0200

    model session

--- delivery_management_system/authentication_service/auth/models.py
+++ delivery_management_system/authentication_service/auth/models.py
@@ -1,7 +1,16 @@
+from datetime import (
+    datetime,
+    timedelta
+)
+
+from typing import List
+
 from sqlalchemy import (
     String,
     Integer,
-    ForeignKey
+    ForeignKey,
+    relationship,
+    DateTime
 )
 from sqlalchemy.orm import (
     Mapped,
@@ -22,6 +31,14 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(50))
     email: Mapped[str]
     password_hash: Mapped[str]
+    sessions: Mapped[List["Session"]] = relationship(back_populates="user")
 
+
+class Session(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    token: Mapped[str] = mapped_column(String, nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow() + timedelta(minutes=30))
+    user: Mapped["User"] = relationship(back_populates="sessions")
 
 
