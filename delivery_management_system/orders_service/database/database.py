dfed0e06ae7f64cd95138fa5b493f5dea0c3aec6
JustGithubProject <Kropivadanil@gmail.com>
2024-03-24 20:53:15 +0200

    imported correctly config path

--- delivery_management_system/orders_service/database/database.py
+++ delivery_management_system/orders_service/database/database.py
@@ -3,8 +3,16 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import sessionmaker
 
+from orders_service.config import (
+    ORDER_DB_HOST,
+    ORDER_DB_USER,
+    ORDER_DB_PASS,
+    ORDER_DB_PORT,
+    ORDER_DB_NAME
+)
+
 engine = create_engine(
-    f"postgresql+psycopg2://{AUTH_DB_USER}:{AUTH_DB_PASS}@{AUTH_DB_HOST}:{AUTH_DB_PORT}/{AUTH_DB_NAME}"
+    f"postgresql+psycopg2://{ORDER_DB_USER}:{ORDER_DB_PASS}@{ORDER_DB_HOST}:{ORDER_DB_PORT}/{ORDER_DB_NAME}"
 )
 
 Session = sessionmaker(engine)
