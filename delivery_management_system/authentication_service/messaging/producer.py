b044f24eb1d6670a0831d4bfd46a84aaba285cb1
JustGithubProject <Kropivadanil@gmail.com>
2024-04-17 20:22:17 +0300

    пофиксил штуку с producer при авторизации

--- delivery_management_system/authentication_service/messaging/producer.py
+++ delivery_management_system/authentication_service/messaging/producer.py
@@ -1,15 +1,27 @@
+import logging
+
 import json
 import pika
 from auth.models import User
+from pika.exceptions import AMQPConnectionError
 
 
 class ProducerAuthorization:
+    connection = None  # Атрибут класса для хранения соединения
+
     def __init__(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue="GET_TOKEN_AND_USER", durable=True)
+        try:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue="GET_TOKEN_AND_USER", durable=True)
+        except AMQPConnectionError as e:
+            logging.error(f"Failed to connect to RabbitMQ: {e}")
 
     def send_user_object_and_token_to_services(self, token: str, user: User):
+        if self.connection is None:
+            logging.error("RabbitMQ connection is not established.")
+            return  # Можно обработать ошибку, если соединение не установлено
+
         decoded_token = token.encode()
         data = {
             "token": decoded_token,
@@ -31,7 +43,7 @@ class ProducerAuthorization:
     def __enter__(self):
         return self
 
-    def __exit__(self):
-        self.connection.close()
-
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection is not None:
+            self.connection.close()
 
